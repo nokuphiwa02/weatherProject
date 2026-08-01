5f5e6411864d7018e8aa9075efe1b6427451ea2e
Nokuphiwa Shange <shangenokuphiwa683@gmail.com>
2026-08-01 17:47:32 +0200

    navbar

--- src/Components/navbar/navbar.tsx
+++ src/Components/navbar/navbar.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
+
 import styles from './navbar.module.css'
-import tiktok from "../../assets/Tiktok.jpg";
 import facebook from "../../assets/facebook.png";
-import twitter from "../../assets/Twitter.jpg";
+import twitter from "../../assets/twitter.png";
+
 
 export const Navbar = () => {
   return (
     <div className={styles.navbar}>
-       <div className={styles["iconImage1"]}>
-          <img src={tiktok} alt="Tiktok.jpg" />
-         
-        </div>
+    <div className={styles.iconImage1}>
+     <img src={facebook} alt="Facebook.png" />
+    </div>
         <h1>Weather App</h1>
-          <div className={styles["iconImage2"]}>
-          <img src={facebook} alt="Facebook.png" />
-          <img src={twitter} alt="Twitter.jpg" />
-        </div>
+       <div className={styles.iconImage2}>
+        <img src={twitter} alt="Twitter.png" />
+    </div>
     </div>
   )
 }

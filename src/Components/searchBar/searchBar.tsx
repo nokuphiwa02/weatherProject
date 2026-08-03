d36f530abed96edfd988a33a210ed37ce3a82d01
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-08-03 11:15:20 +0200

    searchIcon

--- src/Components/searchBar/searchBar.tsx
+++ src/Components/searchBar/searchBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './searchBar.module.css'
+import searchIcon from "../../assets/searchIcon.png";
 
 export const SearchBar = () => {
   return (
     <div className={styles.searchBar}>
       <input type="text" placeholder="Search..." className={styles.searchInput} />
+      <img src={searchIcon} alt="Search Icon" className={styles.searchIcon} />
     </div>
   )
 }
--- src/Components/weatherCard/weatherCard.tsx
+++ src/Components/weatherCard/weatherCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './weatherCard.module.css'
 
 export const WeatherCard = () => {

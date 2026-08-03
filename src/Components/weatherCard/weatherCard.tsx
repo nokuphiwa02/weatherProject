df1c58205070d974329be17d7894fe1a7f1c9cb3
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-08-03 10:26:09 +0200

    weatherCard

---
+++ src/Components/weatherCard/weatherCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './weatherCard.module.css'
+
+export const WeatherCard = () => {
+  return (
+    <div className={styles.weatherCard}>
+      Weather Card
+    </div>
+  )
+}
